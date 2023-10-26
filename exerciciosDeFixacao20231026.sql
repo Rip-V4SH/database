cc404e459e98b31e5ee2c7368ce82d173a0eabc1
EA - Igor Fernandes Almeida <igor_fernandes@educadventista.org>
2023-10-26 18:12:51 -0300

    exercicos 01 à 03 feitos

---
+++ exerciciosDeFixacao20231026.sql
@@ -0,0 +1,35 @@
+-- 01)
+
+delimiter //
+
+create trigger after_insert_cliente after insert on Clientes for each row
+begin
+	insert into Auditoria(mensagem) values('Novo cliente adicionado!!')
+end //
+
+delimiter ;
+
+-- 02)
+
+delimiter //
+
+create trigger before_delete_cliente before delete on Clientes for each row
+begin
+	insert into Auditoria(mensagem) values('tentaram excluir um cliente!!')
+end //
+
+delimiter ;
+
+-- 03)
+
+delimiter //
+
+create trigger after_update_cliente after update on Clientes for each row
+begin
+	insert into Auditoria(mensagem) values(concat('mudaram um nome!! de ', old.nome,' para ', new.nome))
+end //
+
+delimiter ;
+
+-- 04)
+
